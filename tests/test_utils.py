3e5adbcffc79c50452b5bdda64cb154bb5fa7115
Wagner Macedo <wagnerluis1982@gmail.com>
2018-12-23 09:53:23 -0300

    Add tests for rule handling functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,10 @@
 
 """Tests for `ip_liberator.utils` package."""
 
+import typing
 import unittest.mock
 
-from ip_liberator.utils import whats_my_ip
+from ip_liberator.utils import whats_my_ip, make_keys, duplicate_removal, make_rule, make_rules
 
 
 class TestWhatsMyIp:
@@ -38,3 +39,64 @@ class TestWhatsMyIp:
             assert whats_my_ip(value) == value
             assert hasattr(whats_my_ip, 'cache')
             assert whats_my_ip.cache == value
+
+
+def test_make_keys():
+    rule = {
+        'IpPermissions': [make_rule('Python', '10.0.0.1/32', port=1),
+                          make_rule('IsCool', '10.0.0.2/32', from_port=2, to_port=9)]
+    }
+    expected = {
+        (1, 1, 'Python', '10.0.0.1/32'): 0,
+        (2, 9, 'IsCool', '10.0.0.2/32'): 1,
+    }
+
+    assert make_keys(rule) == expected
+
+
+def test_duplicate_removal():
+    rule1 = {
+        'IpPermissions': [make_rule('Python', '10.0.0.1/32', port=1),
+                          make_rule('IsCool', '10.0.0.2/32', port=2)]
+    }
+    rule2 = {
+        'IpPermissions': [make_rule('Python', '10.0.0.1/32', port=1),
+                          make_rule('Second', '10.0.0.2/32', port=2),
+                          make_rule('ToNone', '10.0.0.3/32', port=3)]
+    }
+
+    duplicate_removal(rule1, rule2)
+    assert len(rule1['IpPermissions']) == 1
+    assert len(rule2['IpPermissions']) == 2
+    assert rule1['IpPermissions'] == [make_rule('IsCool', '10.0.0.2/32', port=2)]
+    assert rule2['IpPermissions'] == [make_rule('Second', '10.0.0.2/32', port=2),
+                                      make_rule('ToNone', '10.0.0.3/32', port=3)]
+
+
+def test_make_rule():
+    rule_http = make_rule('HTTP', '10.0.0.1/32', port=80)
+    rule_many = make_rule('Many Services', '10.0.0.1/32', from_port=1, to_port=1000)
+
+    assert rule_http == {'FromPort': 80, 'ToPort': 80, 'IpProtocol': 'tcp',
+                         'IpRanges': [{'CidrIp': '10.0.0.1/32', 'Description': 'HTTP'}]}
+    assert rule_many == {'FromPort': 1, 'ToPort': 1000, 'IpProtocol': 'tcp',
+                         'IpRanges': [{'CidrIp': '10.0.0.1/32', 'Description': 'Many Services'}]}
+
+
+@unittest.mock.patch('ip_liberator.utils.whats_my_ip')
+def test_make_rules(mock_ip):
+    mock_ip.return_value = '10.0.0.1/32'
+
+    rules = make_rules(config={"security_groups": ["sg-1"]},
+                       services={"John SFTP": {"name": "SFTP", "port": "22"},
+                                 "John HTTP": {"name": "HTTP", "port": "80"}})
+    expected = {'GroupId': 'sg-1',
+                'IpPermissions': [
+                    {'FromPort': 22, 'ToPort': 22, 'IpProtocol': 'tcp',
+                     'IpRanges': [{'CidrIp': '10.0.0.1/32', 'Description': 'John SFTP'}]},
+                    {'FromPort': 80, 'ToPort': 80, 'IpProtocol': 'tcp',
+                     'IpRanges': [{'CidrIp': '10.0.0.1/32', 'Description': 'John HTTP'}]}
+                ]}
+
+    assert isinstance(rules, typing.Iterator)
+    assert next(rules) == expected
