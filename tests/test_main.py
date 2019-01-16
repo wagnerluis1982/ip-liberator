1eb315b27b7807007958a44844b1ea5d3ea2a53f
Wagner Macedo <wagnerluis1982@gmail.com>
2019-01-15 21:28:07 -0300

    Add simple test for main function

---
+++ tests/test_main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""Tests for `ip_liberator.__main__` package."""
+import json
+import os
+import tempfile
+from unittest import mock
+
+from ip_liberator.__main__ import main, make_services_index
+from ip_liberator.utils import make_rules
+
+IP = '127.0.0.1/8'
+
+
+@mock.patch('ip_liberator.__main__.AwsIpLiberator')
+@mock.patch('ip_liberator.__main__.print')
+@mock.patch('ip_liberator.utils.whats_my_ip', lambda: IP)
+class TestMain:
+    def setup_method(self):
+        (self.fd, self.filename) = tempfile.mkstemp()
+
+    def teardown_method(self):
+        os.remove(self.filename)
+
+    def test_main(self, mock_print, mock_aws_class):
+        # given
+        access_key = "LT7F9TDQ"
+        secret_key = "SAERB0DITUERDQTYYX8Q"
+        region_name = "sa-west-1"
+        operator = "Fighter"
+        services = [{"name": "HTTP", "port": "80"}]
+        security_groups = ["sg-1"]
+
+        # given
+        settings = make_settings(access_key, secret_key, region_name, operator, services, security_groups)
+        index = make_services_index(settings)
+        rule = next(make_rules(index, settings['config']))
+
+        # given
+        with os.fdopen(self.fd, mode='w') as file:
+            json.dump(settings, file)
+
+        # when
+        main(args=["--profile", self.filename])
+
+        # then
+        mock_print.assert_has_calls([mock.call("Authorizing rules", [svc for svc in index], "to IP", IP),
+                                     mock.call('-', security_groups[0])])
+
+        # then
+        mock_aws_class.assert_called_once_with(access_key, secret_key, region_name)
+        mock_liberator = mock_aws_class.return_value
+        mock_liberator.describe_rules.assert_called_once_with(index, settings['config'])
+        mock_liberator.authorize_rule.assert_called_once_with(rule)
+
+
+def make_settings(access_key: str = None, secret_key: str = None, region_name: str = None,
+                  operator: str = None, services: list = None, security_groups: list = None):
+    return {
+        "credentials": {
+            "access_key": access_key or '',
+            "secret_key": secret_key or '',
+            "region_name": region_name or '',
+        },
+        "config": {
+            "operator": operator or '',
+            "services": services or [{"name": "example", "port": "255"}],
+            "security_groups": security_groups or ["sg-x"],
+        }
+    }
